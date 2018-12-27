db62247bd4bdaab208e1577d30325af75f2d0339
Marc Nuri <marc@marcnuri.com>
2018-12-27 21:52:32 +0100

    Added expanded menu

--- src/components/top-bar/top-bar.jsx
+++ src/components/top-bar/top-bar.jsx
@@ -7,6 +7,15 @@ const TopBar = () => (
       <section className={'mdc-top-app-bar__section mdc-top-app-bar__section--align-start'}>
         <span className={'mdc-top-app-bar__title title'}>JopiSoft</span>
       </section>
+      <section className={'mdc-top-app-bar__section mdc-top-app-bar__section--align-end'}>
+        <ul className={'expanded-menu'}>
+          <li>team</li>
+          <li>activities</li>
+          <li>news</li>
+          <li>media</li>
+          <li>blogs</li>
+        </ul>
+      </section>
     </div>
   </header>
 );
