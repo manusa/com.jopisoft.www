52d20f0f786e4d8bfa3f9a2bb54d320e56846573
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-28 13:47:15 +0000

    Deploy manusa/jopisoft to github.com/manusa/jopisoft.git:gh-pages

--- pages-manifest-403b423b5f9542f0bb81.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{133:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-jsx",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
-//# sourceMappingURL=pages-manifest-403b423b5f9542f0bb81.js.map
