10023adb375e31b5a3c9f08b78a03bc6303f1cbf
Marc Nuri <marc@marcnuri.com>
2019-02-17 07:45:07 +0100

    Jopisoft responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,24 @@ import MainLayout from '../layouts/microjopi/main-layout';
 import './index.scss';
 import Logo from '../components/microjopi/logo/logo';
 
-export const MAIN_TITLE = 'JopiSoft - Where software development meets god';
+export const TITLE = 'JopiSoft';
+export const DESCRIPTION = 'Where software development meets god';
+export const MAIN_TITLE = `${TITLE} - ${DESCRIPTION}`;
+export const MAIN_URL = 'https://www.jopisoft.com';
 
 const Index = () => (
   <MainLayout>
     <Helmet>
       <title>{MAIN_TITLE}</title>
+      <meta name='twitter:url' content={MAIN_URL} />
+      <meta property='og:url' content={MAIN_URL} />
+      <meta name='twitter:title' content={TITLE} />
+      <meta property='og:title' content={TITLE} />
+      <meta name='twitter:description' content={DESCRIPTION} />
+      <meta property='og:description' content={DESCRIPTION} />
+      <meta name='description' content={DESCRIPTION} />
+      <meta property='og:type' content='website' />
+      <meta name='twitter:site' content='@MarcNuri'/>
     </Helmet>
     <div className={'banner'}>
       <Logo size={100}/> Jopisoft
